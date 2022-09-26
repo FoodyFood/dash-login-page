16c5d600856263ee7af9dc4a0b7c7905640af429
doonnor <doconnor@numerix.com>
2022-09-26 10:56:20 +0000

    init commit

---
+++ create-database.py
@@ -0,0 +1,30 @@
+import sqlite3
+from sqlalchemy import Table, create_engine
+from flask_sqlalchemy import SQLAlchemy
+
+conn = sqlite3.connect('data.sqlite')
+
+
+#connect to the database
+engine = create_engine('sqlite:///data.sqlite')
+db = SQLAlchemy()
+
+
+#class for the table Users
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable = False)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+Users_tbl = Table('users', Users.metadata)
+
+
+#fuction to create table using Users class
+def create_users_table():
+    Users.metadata.create_all(engine)
+
+
+#create the table
+create_users_table()
---
+++ display-database-contents.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+from sqlalchemy import Table, create_engine
+from flask_sqlalchemy import SQLAlchemy
+
+
+conn = sqlite3.connect('data.sqlite')
+
+c = conn.cursor()
+df = pd.read_sql('select * from users', conn)
+print(df)
